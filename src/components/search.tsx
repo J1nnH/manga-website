4fe08b9211bf8c6c5e18bdcea78d2765ab1bb77a
github_Neo0724 <neoben007@gmail.com>
2024-05-27 17:53:49 +0800

    Modified header's styling

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,13 +1,4 @@
 "use client";
-import {
-  Dialog,
-  DialogContent,
-  DialogClose,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -22,43 +13,27 @@ export default function Search() {
   };
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <button className="flex gap-3 text-center justify-center items-center">
-          <Image
-            src="/magnifier.png"
-            alt="search"
-            width={32}
-            height={32}
-            className="self-center"
-          />
-          <span className="hidden md:block">Search</span>
-        </button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Search Anime by title</DialogTitle>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Title
-            </Label>
-            <Input
-              id="title"
-              defaultValue=""
-              className="col-span-3"
-              value={searchMangaTitle}
-              onChange={(e) => handleChangeTitle(e)}
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button type="submit">Search</Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <form className="flex gap-5" action="">
+      <label htmlFor="searchManga" className="flex w-[64px]">
+        <Image
+          src="/magnifier.png"
+          alt="search"
+          width={32}
+          height={32}
+          className="self-center"
+        />
+      </label>
+      <Input
+        id="searchManga"
+        type="text"
+        placeholder="Search for anime ..."
+        className="bg-transparent text-md"
+        value={searchMangaTitle}
+        onChange={(e) => handleChangeTitle(e)}
+      />
+      <Button type="submit" variant="secondary">
+        Search
+      </Button>
+    </form>
   );
 }
