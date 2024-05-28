be2cc9a92564d73535113d519418d7a39c2fda9b
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-05-28 19:51:39 +0800

    add read more function

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,16 +1,24 @@
+"use client";
+
 import { mangadex } from "@/components/mangaDexInstance";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from 'react';
 
-export default async function Ranking() {
-  const popular = await mangadex.fetchPopular();
-
+export default function Ranking() {
+  const popular = mangadex.fetchPopular()
+  .then((popular) => {
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
     <main className="min-h-screen  bg-slate-900 text-white pt-10 pl-10 pr-10">
       <h1 className="text-xl font-bold inline pl-6">Most Popular</h1>
       <ul>
         {popular.results.map(async (manga, index) => {
           const mangaInfo = await mangadex.fetchMangaInfo(manga.id);
+          
+          const MAX_LENGTH = 100;
+          const toggleReadMore = () => {setIsExpanded(!isExpanded);};
+
           return (
             <li
               className="py-5 m-5 bg-black border border-gray-500 rounded-md"
@@ -27,13 +35,20 @@ export default async function Ranking() {
                   />
                   <div>
                     <p>Title: {manga.title as string}</p>
-                    <p>Status: {manga.status}</p>
+                    <p>Genre: {mangaInfo.genres?.join(', ')}</p>
                     <p>Year: {manga.releaseDate}</p>
-                    <p>Genre: {mangaInfo.genres}</p>
+                    <p>Status: {manga.status}</p>
+                    <br />
                     <hr />
                     <br />
                     <p>Description: </p>
                     <p>{manga.description as string}</p>
+                    <p>
+                      {isExpanded ? manga.description as string: (manga.description as string).slice(0,MAX_LENGTH)}
+                    </p>
+                    <button onClick={toggleReadMore} className="text-blue-500 hover:text-blue-700">
+                      {isExpanded ? "Read less":"Read more"}
+                    </button>
                   </div>
                 </div>
               </Link>
@@ -43,5 +58,7 @@ export default async function Ranking() {
       </ul>
     </main>
   );
+  })
+  
 }
 
