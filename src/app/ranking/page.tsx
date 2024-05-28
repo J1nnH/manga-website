b0ad53dd9ba706d573201a99d1858b88cefcadd6
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-05-28 11:39:15 +0800

    small update on UI

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -7,18 +7,18 @@ export default async function Ranking(){
 
 
     return (
-        <main className="min-h-screen  bg-slate-900 text-white">
+        <main className="min-h-screen  bg-slate-900 text-white pt-10 pl-10 pr-10">
            
-            <h1 className="text-xl font-bold inline">Most Popular</h1>
+            <h1 className="text-xl font-bold inline pl-6">Most Popular</h1>
             <ul >
                 {popular.results.map(async (manga,index) =>{
                     const mangaInfo = await mangadex.fetchMangaInfo(manga.id);
                     return(
-                        <li className="py-5 m-5 bg-black border border-gray-500">
+                        <li className="py-5 m-5 bg-black border border-gray-500 rounded-md">
                             <Link href={`manga/${manga.id}`}>
                                 <div className="flex space-x-4">
-                                    <div className="items-center">{index + 1}</div>
-                                    <img src={mangaInfo.image as string} width={200} height={400}></img>
+                                    <div className="items-center pt-32 pl-5 pr-2 font-bold">{index + 1}</div>
+                                    <img src={mangaInfo.image as string} width={200} height={400} className="rounded-md"></img>
                                     <div>
                                         <p>Title: {manga.title as string}</p>
                                         <p>Status: {manga.status}</p>
