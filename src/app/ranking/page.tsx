499d06ada80fe519b2a86646331b8be72da25f3c
Sodiumchloridy <bryanlee13579@gmail.com>
2024-05-28 21:22:01 +0800

    chore: Refactor Ranking page to improve performance and error handling

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -3,23 +3,8 @@ import MangaItem from "./mangaItem";
 import { IMangaResult } from "@consumet/extensions";
 export const revalidate = 3600;
 
-// This function will retry fetching the popular manga list n times with an exponential backoff delay
-async function fetchWithRetry(
-  fetchFunction: Function,
-  retries: number,
-  delay: number
-) {
-  try {
-    return await fetchFunction();
-  } catch (err) {
-    if (retries === 1) throw err;
-    await new Promise((resolve) => setTimeout(resolve, delay));
-    return fetchWithRetry(fetchFunction, retries - 1, delay * 2);
-  }
-}
-
 export default async function Ranking() {
-  const popular = await fetchWithRetry(() => mangadex.fetchPopular(), 3, 1000);
+  const popular = await mangadex.fetchPopular();
   return (
     <main className="min-h-screen  bg-slate-900 text-white pt-10 pl-10 pr-10">
       <h1 className="text-xl font-bold inline pl-6">Most Popular</h1>
