faecb4a658ea48e3c7a795cdb633fe609fe1aeda
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-05-28 20:08:49 +0800

    remove useless import

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { mangadex } from "@/components/mangaDexInstance";
-import { Console } from "console";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from 'react';
