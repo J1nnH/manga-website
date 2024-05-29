0d25cec5990b6859760f9b4e4e4cb8d332968b56
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-05-29 08:30:19 +0800

    added readable comments

--- src/app/ranking/mangaItem.tsx
+++ src/app/ranking/mangaItem.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
 import { IMangaInfo } from "@consumet/extensions";
+
 export default function MangaItem({
   mangaInfo,
   index,
@@ -11,17 +12,25 @@ export default function MangaItem({
   mangaInfo: IMangaInfo;
   index: number;
 }) {
+
+  // Initialize 2 variables using useState, default value for isExpanded is false, setIsExpanded is a setter 
   const [isExpanded, setIsExpanded] = useState(false);
 
+  // Return a manga section
   return (
+
     <li
       className="py-5 m-5 bg-black border border-gray-500 rounded-md"
       key={mangaInfo.id}
     >
       <div className="flex space-x-4 md:flex-row flex-col justify-center items-center">
+
+        {/* The numbering for the manga */}
         <div className="self-start md:self-center pl-5 pr-2 font-bold">
           {index + 1}
         </div>
+
+        {/* Making only the image of manga is clickable, to watch the manga */}
         <Link href={`manga/${mangaInfo.id}`}>
           <Image
             className="aspect-[3/4]"
@@ -31,6 +40,8 @@ export default function MangaItem({
             alt="cover image"
           />
         </Link>
+
+        {/* Listing the information of the manga */}
         <div>
           <p>Title: {mangaInfo.title as string}</p>
           <p>Genre: {mangaInfo.genres?.join(", ")}</p>
@@ -39,6 +50,8 @@ export default function MangaItem({
           <br />
           <hr />
           <br />
+
+          {/* Making Read more and Read less function for the long description */}
           <p>Description: </p>
           <p className={isExpanded ? "" : "line-clamp-1"}>
             {(mangaInfo.description as { en: string })?.en}
