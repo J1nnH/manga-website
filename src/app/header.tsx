91b53767a8f2f3ffdb945533a4bec2c9e2fd0443
Sodiumchloridy <bryanlee13579@gmail.com>
2024-06-05 21:51:43 +0800

    refactor: Update header UI and navigation links for mobile view

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Search from "./(components)/search";
 import {
@@ -9,6 +10,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const LoginRegister = dynamic(
   () => import("./(components)/login-register-nav"),
@@ -20,9 +22,20 @@ const LoginRegister = dynamic(
 const navigation = ["manga", "ranking", "favourites", "login", "about"];
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className="flex flex-col bg-gray-800 text-white p-4 justify-between items-center w-full">
-      <div className="flex align-middle justify-center items-center w-full">
+    <header className="flex flex-col bg-gray-800 text-white px-2 w-full">
+      <div className="flex w-full items-center">
+        {/** Hamburger menu Icon */}
+        <svg
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          className="h-6 fill-white aspect-square mx-4 md:hidden"
+        >
+          <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+        </svg>
         <a href="/">
           <Image
             src="/manga.png"
@@ -32,8 +45,8 @@ export default function Header() {
             priority={true}
           />
         </a>
-        <nav className="flex-grow m-2 w-[30%] overflow-auto scrollbar ml-20">
-          <ul className="flex text-lg uppercase space-x-2">
+        <nav className="flex-grow m-2 w-[30%] overflow-auto scrollbar hidden md:block">
+          <ul className="text-lg uppercase space-x-2 flex">
             {navigation.map((title) => {
               let path = `/${title}`;
               if (title == "manga") {
@@ -43,17 +56,18 @@ export default function Header() {
               }
 
               return (
-                <a href={path}>
-                  <li className="relative min-w-[fit-content] py-10 px-5 transition-all cursor-pointer group hover:text-gray-400 active:bg-gray-700 rounded-lg">
+                <Link href={path}>
+                  <li className="relative min-w-[fit-content] py-0 px-2 transition-all cursor-pointer group hover:text-gray-400 active:bg-gray-700 rounded-lg">
                     <span className="absolute inset-x-0 bottom-0 h-1 bg-transparent group-hover:bg-gray-400 transition-all"></span>
                     {title}
                   </li>
-                </a>
+                </Link>
               );
             })}
           </ul>
         </nav>
 
+        {/** Search bar */}
         <Search />
 
         <div className="flex justify-center py-2 text-white gap-3 flex-wrap w-[15%] max-w-[250px]">
@@ -80,6 +94,49 @@ export default function Header() {
           <Search />
         </li>
       </ul> */}
+
+      {/** Sidebar Menu on Mobile View */}
+      {isMenuOpen && (
+        <>
+          <div
+            onClick={() => setIsMenuOpen(false)}
+            className="z-10 fixed h-screen w-screen backdrop-blur	"
+          />
+          <nav className="z-10 md:hidden fixed h-full left-0 w-[70vw] bg-black p-6 justify-center">
+            <div className="w-full mb-8 justify-between flex">
+              <h1 className="font-bold text-2xl uppercase">Menu</h1>
+              {/** Close icon */}
+              <svg
+                onClick={() => setIsMenuOpen(false)}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+                className="aspect-square w-8 fill-white"
+              >
+                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+              </svg>
+            </div>
+
+            <ul className="gap-4 grid grid-cols-1">
+              {navigation.map((title) => {
+                let path = `/${title}`;
+                if (title == "manga") {
+                  path = "/";
+                } else if (title == "login") {
+                  path = `/${title}-manga`;
+                }
+
+                return (
+                  <Link href={path} onClick={() => setIsMenuOpen(false)}>
+                    <li className="text-xl uppercase border-l-indigo-500 border-l-4 pl-2">
+                      {title}
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
+          </nav>
+        </>
+      )}
     </header>
   );
 }
