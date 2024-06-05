b3d91223da55be60b051c12cb55e81c5836f2d9f
Sodiumchloridy <bryanlee13579@gmail.com>
2024-06-05 20:59:43 +0800

    chore: change search icon from image to svg

--- src/app/(components)/search.tsx
+++ src/app/(components)/search.tsx
@@ -3,7 +3,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import Image from "next/image";
 import { useState } from "react";
 
 export default function Search() {
@@ -16,19 +15,7 @@ export default function Search() {
   };
 
   return (
-    <form
-      className="flex gap-5 mr-50"
-      action={`/search-result/${searchMangaTitle}`}
-    >
-      <label htmlFor="searchManga" className="flex w-[64px]">
-        <Image
-          src="/magnifier.png"
-          alt="search"
-          width={32}
-          height={32}
-          className="self-center"
-        />
-      </label>
+    <form className="flex gap-5" action={`/search-result/${searchMangaTitle}`}>
       <Input
         id="searchManga"
         type="text"
@@ -38,6 +25,12 @@ export default function Search() {
         onChange={(e) => handleChangeTitle(e)}
       />
       <Button type="submit" variant="secondary">
+        <label htmlFor="searchManga" className="aspect-square w-4 mr-2">
+          {/* Search icon */}
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+          </svg>
+        </label>
         Search
       </Button>
     </form>
