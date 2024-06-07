0c70cda73e4835ac5b3ec07382ffabff8f540bf2
Neo0724 <neoben007@gmail.com>
2024-06-07 12:56:31 +0800

    Fixed favourite page's contents not loading

--- src/app/[locale]/favourites/page.tsx
+++ src/app/[locale]/favourites/page.tsx
@@ -1,16 +1,26 @@
 import initTranslations from "../../i18n";
-import TranslationProvider from "../(components)/TranslationProvider"
+import TranslationProvider from "../(components)/TranslationProvider";
 import FavouritesPage from "./favouritesItem";
 
-const i18nNamespaces = ['favourites','common'];
+const i18nNamespaces = ["favourites", "common"];
 
-export default async function Favourites({params}: {params: {locale: string; searchTitle: string}}){
-  const { t, resources } = await initTranslations(params.locale, i18nNamespaces);
+export default async function Favourites({
+  params,
+}: {
+  params: { locale: string; searchTitle: string };
+}) {
+  const { t, resources } = await initTranslations(
+    params.locale,
+    i18nNamespaces
+  );
 
   return (
-    <TranslationProvider resources={resources} locale={params.locale} namespaces={i18nNamespaces}>
-      <FavouritesPage></FavouritesPage>
+    <TranslationProvider
+      resources={resources}
+      locale={params.locale}
+      namespaces={i18nNamespaces}
+    >
+      <FavouritesPage />
     </TranslationProvider>
-
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,22 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { i18nRouter } from 'next-i18n-router';
-import i18nConfig from './i18nConfig';
+import { NextRequest } from "next/server";
+import { i18nRouter } from "next-i18n-router";
+import i18nConfig from "./i18nConfig";
 
 export function middleware(request: NextRequest) {
-
-  if (request.nextUrl.pathname.startsWith('/favourites')) {
-    
+  if (request.nextUrl.pathname.startsWith("/favourites")) {
     // This logic is only applied to /favourites
     const loggedIn = request.cookies.get("userId");
-  
-    if (!loggedIn) {
+
+    if (!loggedIn?.value) {
       return NextResponse.redirect(new URL("/login-manga", request.url));
+    } else {
+      NextResponse.next();
     }
-  }else{
-    return i18nRouter(request, i18nConfig);
   }
+  return i18nRouter(request, i18nConfig);
 }
 
 export const config = {
-  matcher: ["/favourites", '/((?!api|static|.*\\..*|_next).*)']
+  matcher: ["/favourites", "/((?!api|static|.*\\..*|_next).*)"],
 };
-
-
