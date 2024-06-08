6ba526ba2befb4ce99355cb413235b6b702eff25
J1nnH <125129010+J1nnH@users.noreply.github.com>
2024-06-08 11:50:33 +0800

    made the language selector works

---
+++ src/app/[locale]/(components)/LanguageChanger.js
@@ -0,0 +1,73 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+import { useTranslation } from 'react-i18next';
+import i18nConfig from '@/i18nConfig';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select";
+
+import Image from "next/image";
+
+export default function LanguageChanger() {
+  const { i18n } = useTranslation();
+  const currentLocale = i18n.language;
+  const router = useRouter();
+  const currentPathname = usePathname();
+
+  const handleChange = e => {
+    const newLocale = e.target.value;
+
+    // set cookie for next-i18n-router
+    const days = 30;
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = date.toUTCString();
+    document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`;
+
+    // redirect to the new locale path
+    if (
+      currentLocale === i18nConfig.defaultLocale &&
+      !i18nConfig.prefixDefault
+    ) {
+      router.push('/' + newLocale + currentPathname);
+    } else {
+      router.push(
+        currentPathname.replace(`/${currentLocale}`, `/${newLocale}`)
+      );
+    }
+
+    router.refresh();
+  };
+
+  return (
+    <select onChange={handleChange} value={currentLocale} className="text-black">
+      <option value="en">English</option>
+      <option value="ms">Bahasa Melayu</option>
+      <option value="zh">Chinese</option>
+    </select>
+//     <div className="flex justify-center py-2 text-white gap-3 flex-wrap w-[15%] max-w-[250px]">
+//     <Select defaultValue="en" onChange={handleChange} value={currentLocale}>
+//       <SelectTrigger>
+//         <Image
+//           src="/language.png"
+//           alt="language icon"
+//           width={32}
+//           height={32}
+//         />
+//         <SelectValue placeholder="Language" />
+//       </SelectTrigger>
+//       <SelectContent >
+//         <SelectItem value="en">English</SelectItem>
+//         <SelectItem value="ms">Bahasa Melayu</SelectItem>
+//         <SelectItem value="zh">Chinese</SelectItem>
+//       </SelectContent>
+//     </Select>
+//   </div>
+  );
+}
