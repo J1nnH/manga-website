e7b08a89b4b6d6d58a6c738aeb9aa4be5cc66de6
Sodiumchloridy <67376832+Sodiumchloridy@users.noreply.github.com>
2024-07-29 23:42:17 +0800

    update site name

--- src/app/[locale]/footer.tsx
+++ src/app/[locale]/footer.tsx
@@ -1,15 +1,12 @@
-"use client"
+"use client";
 import { useTranslation } from "react-i18next";
 
 export default function Footer() {
-
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
     <div className="grid place-items-center p-4 bg-slate-800 text-white text-sm">
-      <p>
-        {t("footer")}
-      </p>
-      <p>Copyright &copy;{new Date().getFullYear()} Rush B</p>
+      <p>{t("footer")}</p>
+      <p>Copyright &copy;{new Date().getFullYear()} Lovat</p>
     </div>
   );
 }
